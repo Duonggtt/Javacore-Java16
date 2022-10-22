e1c115e7c32018b03ed11678bfac5d1959e96b76
Duonggtung <duonggtung173@gmail.com>
2022-10-22 18:23:58 +0700

    BTVN L1

--- LESSON_1/src/Bai4.java
+++ LESSON_1/src/Bai4.java
@@ -2,7 +2,8 @@ public class Bai4 {
     public  static  void main(String[] args) {
         //Bài 4: Khai căn một số dương bất kì, kết quả làm tròn tới 3 chữ số thập phân.
 
-        double num = Math.sqrt(20);
+        double giatri = 20;
+        double num = Math.sqrt(giatri);
         System.out.println("Result = " + (double) Math.ceil(num * 1000) / 1000);
     }
 }
